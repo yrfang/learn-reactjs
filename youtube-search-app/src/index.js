12a7aa642687af32bc510ec6d8b2eaf9c4784dd9
yrfang <fyr1116@gmail.com>
2017-09-04 22:25:24 +0800

    setState of data

--- youtube-search-app/src/index.js
+++ youtube-search-app/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBDiWzPNDBNVZJ5y8_cbZnrihaF255wAco';
+
 const App = () => {
-  return <div>Hi!!!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
