98f29c3c9cf485790bbb83103ea7a7d32100f006
yrfang <fyr1116@gmail.com>
2017-09-12 12:10:02 +0800

    connect react and redux library

--- book_list/src/containers/book-list.js
+++ book_list/src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
@@ -10,10 +11,22 @@ export default class BookList extends Component {
   }
 
   render() {
+    // console.log(this.props.asdf) // => 123
     return (
       <ul className="list-group col-sm-4">
         {this.renderList()}
       </ul>
-    ;
+    );
   }
 }
+
+function mapStateToProps(state) {
+  // Whatever is returned will show up as props
+  // inside of BookList
+  return {
+    // asdf: '123'
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
