3c43ee137678001229ca2f32ad659dd20d68f2ae
yrfang <fyr1116@gmail.com>
2017-09-13 21:18:52 +0800

    avioding state mutations in reducers

--- weather/src/reducers/reducer_weather.js
+++ weather/src/reducers/reducer_weather.js
@@ -4,6 +4,7 @@ export default function(state=[], action) {
   switch (action.type) {
     case: FETCH_WEATHER:
       return state.concat([action.payload.data]);
+      //return [ action.payload.data, ...state ]; //it's identical in ES6
   }
 
   return state;
