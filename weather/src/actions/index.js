26bd435cf953ab123cdff12046442e639f87f4ea
yrfang <fyr1116@gmail.com>
2017-09-13 19:02:58 +0800

    redux-promise in practice

--- weather/src/actions/index.js
+++ weather/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '10b96a555cb951f83ebe6df77799ca60';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?q=London,us&appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
