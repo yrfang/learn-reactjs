5d6eca4067220f3bb4c52f1d345e7decd5ca2884
yrfang <fyr1116@gmail.com>
2017-09-13 22:17:46 +0800

    building a list container

--- weather/src/reducers/reducer_weather.js
+++ weather/src/reducers/reducer_weather.js
@@ -1,11 +1,9 @@
 import { FETCH_WEATHER } from '../actions/index';
 
-export default function(state=[], action) {
+export default function(state = [], action) {
   switch (action.type) {
-    case: FETCH_WEATHER:
-      return state.concat([action.payload.data]);
-      //return [ action.payload.data, ...state ]; //it's identical in ES6
+    case FETCH_WEATHER:
+      return [action.payload.data, ...state];
   }
-
   return state;
 }
