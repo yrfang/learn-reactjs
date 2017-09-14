06f144255b884e6b5aa6389717a84f3253022996
yrfang <fyr1116@gmail.com>
2017-09-14 23:24:32 +0800

    reduxof index action

--- blog/src/index.js
+++ blog/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostIndex from './components/post_index';
