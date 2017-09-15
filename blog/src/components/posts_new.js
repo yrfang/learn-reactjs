c1078d095dc1c5d9d5c2317a64e1ccd2f1641bed
yrfang <fyr1116@gmail.com>
2017-09-15 17:15:08 +0800

    the field component

--- blog/src/components/posts_new.js
+++ blog/src/components/posts_new.js
@@ -3,12 +3,23 @@ import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 
 class PostsNew extends Component {
+  renderTitleField(field) {
+    return (
+      <div>
+        <input
+          type="text"
+          {...field.input} //onChange={field.input.onChange}
+        />
+      </div>
+    );
+  }
+
   render() {
     return (
       <form>
         <Field
           name="title"
-          component={}
+          component={this.renderTitleField}
         />
       </form>
     );
