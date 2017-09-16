c5177f09c2a9aaeac24507619e62d225ffe53090
yrfang <fyr1116@gmail.com>
2017-09-16 17:42:59 +0800

    receiving new posts

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POST = 'create_post';
+export const FETCH_POST = 'fetch_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=PAPERCLIP1234';
@@ -21,6 +22,15 @@ export function createPost(values, callback) {
 
   return {
     type: CREATE_POST,
+    payload: request
+  }
+}
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
     payload: request
   }
 }
