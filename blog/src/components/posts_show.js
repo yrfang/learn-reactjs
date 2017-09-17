bc8013473042768831e45bd8bce41488404ae053
yrfang <fyr1116@gmail.com>
2017-09-17 11:59:15 +0800

    selecting from ownProps

--- blog/src/components/posts_show.js
+++ blog/src/components/posts_show.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+  componentDidMount() {
+    //we just want to fetch the particular id in url, not a lists of posts
+    //match the id in the url implemented by Route
+    // this.props.match.params.id;
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
   render() {
+    // this.props === ownProps
     return (
       <div>
         Posts Show!
@@ -10,4 +21,8 @@ class PostsShow extends Component {
   }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
