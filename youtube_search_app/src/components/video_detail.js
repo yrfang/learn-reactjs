32dc4c99abf371c3f6e382d3199aa6f58dc82dc4
yrfang <fyr1116@gmail.com>
2017-09-23 21:36:29 +0800

    -.-

--- youtube_search_app/src/components/video_detail.js
+++ youtube_search_app/src/components/video_detail.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const VideoDetail = ({video}) => {
   if (!video) {
-    return <div>Loading...</div>;
+    return (
+      <div>Loading...</div>
+    );
   }
 
   const videoId = video.id.videoId;
